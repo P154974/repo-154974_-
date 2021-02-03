61edb787dcf46dac7217605999e1cc09a575785d
P154974 <75369795+P154974@users.noreply.github.com>
2021-02-03 15:46:58 +0100

    Add files via upload

--- mysin.cpp
+++ mysin.cpp
@@ -1,1 +1,15 @@
 #include "mysin.h"
+#include <stdio.h>
+
+class MySin {
+
+public:
+
+MySin();
+MySin(double x);
+MySin(const MySin &obj);
+~MySin();
+double value();
+void setX(double);
+double getX();
+};
